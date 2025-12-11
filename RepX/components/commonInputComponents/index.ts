e5913fd30aec872c1b32bed05f2862f30a8b3316
navnitan-7 <snavnitan@gmail.com>
2025-12-12 03:26:48 +0530

    Remove deprecated files and components from RepX project, including configuration files, styles, and UI components. This cleanup enhances project maintainability and prepares for future development.

--- RepX/components/commonInputComponents/index.ts
+++
@@ -1,4 +0,0 @@
-export { FormTextInput } from './FormTextInput';
-export { FormNumericInput } from './FormNumericInput';
-export { FormSelectableButtons } from './FormSelectableButtons';
-export { FormToggleSwitch } from './FormToggleSwitch'; 
--- RepX/components/eventSpecific/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ParticipantCard } from './ParticipantCard';
-export { default as LeaderboardCard } from './LeaderboardCard';
